760d7736a9728f08085ef32dd478db0269c90053
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-07-19 01:29:51 +0200

    Pdf and Image Scripts

--- .helpers/guake_cd_to.sh
+++ .helpers/guake_cd_to.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-dbus-send --session --type=method_call --dest=org.guake.RemoteControl /org/guake/RemoteControl org.guake.RemoteControl.show_hide
+dbus-send --session --type=method_call --dest=org.guake.RemoteControl /org/guake/RemoteControl org.guake.RemoteControl.show
 dbus-send --session --type=method_call --dest=org.guake.RemoteControl /org/guake/RemoteControl org.guake.RemoteControl.execute_command string:"ccd '$*'"
